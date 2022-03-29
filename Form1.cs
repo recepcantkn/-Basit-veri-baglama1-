e5e838bae0c6c1c91d23a2da24212d13f44129a7
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-29 09:25:55 +0300

    Basit veri bağlama1

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Binding bagla = new Binding("Text", txtKaynak, "Text");
+            txtHedef.DataBindings.Add(bagla);
+        }
+    }
+}
